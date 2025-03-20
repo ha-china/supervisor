15eabf59b73669daf9aa49a16cd6a0303ac479ae
Desmond <djhui@live.cn>
2025-03-20 22:03:30 +0800

    Update validate.py

--- supervisor/store/validate.py
+++ supervisor/store/validate.py
@@ -6,7 +6,7 @@ from ..const import ATTR_MAINTAINER, ATTR_NAME, ATTR_REPOSITORIES, ATTR_URL
 from ..validate import RE_REPOSITORY
 from .const import StoreType
 
-URL_COMMUNITY_ADDONS = "https://github.com/hassio-addons/repository"
+#URL_COMMUNITY_ADDONS = "https://github.com/hassio-addons/repository"
 #URL_ESPHOME = "https://gitee.com/desmond_GT/hassio-addons"
 #URL_MUSIC_ASSISTANT = "https://github.com/music-assistant/home-assistant-addon"
 BUILTIN_REPOSITORIES = {
